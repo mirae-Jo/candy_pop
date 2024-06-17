1df870102b95b2a00ee14d5a873f4e06ea4327d7
대빵 <yuna_park@DAEPPANG-MacBook-Pro.local>
2024-06-17 18:08:46 +0900

    tailwindConfigColor 지정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        ['primary-Fcolor']: 'var(--primary-Fcolor)',
+        ['secondary-Fcolor']: 'var(--secondary-Fcolor)',
+        ['primary-blue']: 'var(--primary-blue)',
+        ['secondary-blue']: 'var(--secondary-blue)',
+        ['primary-pink']: 'var(--primary-pink)',
+        ['secondary-pink']: 'var(--secondary-pink)',
+        ['primary-bg']: 'var(--primary-bg)',
+        ['secondary-bg']: 'var(--secondary-bg)',
+        ['primary-line']: 'var(--primary-line)',
+        ['secondary-line']: 'var(--secondary-line)',
+        ['primary-accent']: 'var(--primary-accent)',
       },
     },
   },
