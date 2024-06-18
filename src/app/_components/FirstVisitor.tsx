3bb92f185a62157f9d823fe7986a55aad105bd1d
mirae-Jo <mirae9873@gmail.com>
2024-06-18 21:19:44 +0900

    first visitor page 만들기

---
+++ src/app/_components/FirstVisitor.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FirstVisitor = () => {
+  return (
+    <div className='bg-[#FFEBF1] w-full h-screen flex items-center justify-center'>
+      <div className='text-center'>
+        <p>한 눈에 보는 팝업스토어</p>
+        <h1>candy pop</h1>
+        <button className='w-[32rem] h-[4.4rem] bg-secondary-pink rounded-16xl'>
+          <span className=' text-white'>시작하기</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FirstVisitor;
