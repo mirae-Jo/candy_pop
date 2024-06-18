c3fa8616958e7dc1d52f7086f60b7f9999f3af54
mirae-Jo <mirae9873@gmail.com>
2024-06-18 21:45:26 +0900

    tailwind config 폰트 사이즈 추가

--- src/app/_components/FirstVisitor.tsx
+++ src/app/_components/FirstVisitor.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const FirstVisitor = () => {
   return (
-    <div className='bg-[#FFEBF1] w-full h-screen flex items-center justify-center'>
-      <div className='text-center'>
+    <div className='bg-[#FFEBF1] w-full h-screen flex items-center'>
+      <div className='flex flex-col'>
         <p>한 눈에 보는 팝업스토어</p>
         <h1>candy pop</h1>
         <button className='w-[32rem] h-[4.4rem] bg-secondary-pink rounded-16xl'>
-          <span className=' text-white'>시작하기</span>
+          <span className=' text-24px text-white'>시작하기</span>
         </button>
       </div>
     </div>
