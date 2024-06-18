f1fc5bf1de701dd70d2d77dc5b884f1d2c56fac0
mirae-Jo <mirae9873@gmail.com>
2024-06-18 22:17:03 +0900

    first visitor page css 완

--- src/app/_components/FirstVisitor.tsx
+++ src/app/_components/FirstVisitor.tsx
@@ -1,13 +1,25 @@
+import Image from 'next/image';
 import React from 'react';
+import candyPopLogo from '@/assets/logo.png';
+import RightArrowSvg from '@/assets/RightArrowSvg';
 
 const FirstVisitor = () => {
   return (
-    <div className='bg-[#FFEBF1] w-full h-screen flex items-center'>
-      <div className='flex flex-col'>
-        <p>한 눈에 보는 팝업스토어</p>
-        <h1>candy pop</h1>
-        <button className='w-[32rem] h-[4.4rem] bg-secondary-pink rounded-16xl'>
-          <span className=' text-24px text-white'>시작하기</span>
+    <div className='bg-[#FFEBF1] w-full h-screen flex items-center justify-center'>
+      <div className='flex flex-col items-center gap-[1.2rem]'>
+        <p className='text-secondary-pink text-16px font-light'>
+          한 눈에 보는 팝업스토어
+        </p>
+        <h1>
+          <Image
+            className='w-[32rem]'
+            src={candyPopLogo}
+            alt='candy pop logo'
+          />
+        </h1>
+        <button className='mt-[0.8rem] w-[32rem] h-[4.4rem] bg-secondary-pink rounded-16xl flex items-center justify-center gap-[1.5rem]'>
+          <span className='text-24px text-white font-light'>시작하기</span>
+          <RightArrowSvg />
         </button>
       </div>
     </div>
