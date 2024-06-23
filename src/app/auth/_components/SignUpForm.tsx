a3e3febb78875d4547975138a3bd63156ec8c705
mirae-Jo <mirae9873@gmail.com>
2024-06-23 23:05:46 +0900

    auth 폴더트리 만들기

---
+++ src/app/auth/_components/SignUpForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SignUpForm = () => {
+  return (
+    <form>
+      <input type='text' />
+      <input type='email' />
+      <input type='password' />
+      <input />
+      <button>회원가입</button>
+    </form>
+  );
+};
+
+export default SignUpForm;
