0b80e2fb136509e5c7d1815e120864a1776b8304
mirae-Jo <mirae9873@gmail.com>
2024-06-25 21:07:30 +0900

    email입력시 magic link 전송

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,23 @@
+import type { Adapter } from 'next-auth/adapters';
 import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
+import { SupabaseAdapter } from '@auth/supabase-adapter';
+import { supabase } from '../../db';
+
+const EMAIL_SERVER = 'smtp://mirae9873:adxvwfgfmznpuphs@smtp.gmail.com:587'; // process.env.EMAIL_SERVER;
+const EMAIL_FROM = 'noreply@candypop.com'; // process.env.EMAIL_FROM;
 
 const handler = NextAuth({
-  providers: [],
+  providers: [
+    EmailProvider({
+      server: EMAIL_SERVER,
+      from: EMAIL_FROM,
+    }),
+  ],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    secret: process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY as string,
+  }) as Adapter,
 });
 
 export { handler as GET, handler as POST };
