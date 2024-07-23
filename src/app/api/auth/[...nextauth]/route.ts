b9df116fdcf9d14bad991949d8e1e79b1df14ed1
mirae-Jo <mirae9873@gmail.com>
2024-07-23 19:04:01 +0900

    social log in 구현

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,32 +1,47 @@
 import type { Adapter } from 'next-auth/adapters';
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
+import NaverProvider from 'next-auth/providers/naver';
+import KakaoProvider from 'next-auth/providers/kakao';
 import { SupabaseAdapter } from '@auth/supabase-adapter';
 
 const EMAIL_SERVER = process.env.EMAIL_SERVER; // process.env.EMAIL_SERVER;
 const EMAIL_FROM = 'noreply@candypop.com'; // process.env.EMAIL_FROM;
 
 const handler = NextAuth({
+  // secret: process.env.AUTH_SECRET,
   providers: [
     EmailProvider({
       server: EMAIL_SERVER,
       from: EMAIL_FROM,
     }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID as string,
+      clientSecret: process.env.NAVER_CLIENT_SECRET as string,
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID as string,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
+    }),
   ],
   callbacks: {
-    async redirect({ url, baseUrl }) {
-      console.log(baseUrl);
-      return baseUrl;
+    // async redirect({ url, baseUrl }) {
+    //   console.log(baseUrl);
+    //   return baseUrl;
+    // },
+    async jwt({ token }) {
+      console.log('token is : ', token);
+      return token;
     },
-    // async jwt({ token, account }) {
-    //   if (account) {
-    //     token.accessToken = account.access_token;
-    //   }
-    //   return token;
-    // },
-    // async session({ session, token }) {
-    //   return session;
-    // },
+    async session(args) {
+      const { session } = args;
+      console.log(args);
+      return session;
+    },
+  },
+  debug: true,
+  pages: {
+    signIn: '/',
   },
   adapter: SupabaseAdapter({
     url: process.env.NEXT_PUBLIC_SUPABASE_URL as string,
--- src/app/auth/_components/SignUpForm.tsx
+++ src/app/auth/_components/SignUpForm.tsx
@@ -1,18 +1,28 @@
 'use client';
 
-import React, { useState } from 'react';
-import { signIn } from 'next-auth/react';
+import React, { useEffect, useState } from 'react';
+import { signIn, useSession } from 'next-auth/react';
 
 const SignUpForm = () => {
   const [email, setEmail] = useState('');
 
+  const { data: session } = useSession();
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    signIn('email', { email });
+    await signIn('email', { email });
+  };
+
+  const handleNaverSignUpBtn = async () => {
+    await signIn('naver');
+  };
+
+  const handleKakaoSignUpBtn = async () => {
+    await signIn('kakao', { redirect: true, callbackUrl: '/' });
   };
 
   return (
-    <>
+    <div className='flex flex-col gap-3'>
       <form onSubmit={handleSubmit} className='flex flex-col gap-[1rem]'>
         <input
           value={email}
@@ -22,7 +32,13 @@ const SignUpForm = () => {
         />
         <button className='bg-[#eee]'>이메일로 시작하기</button>
       </form>
-    </>
+      <button onClick={handleNaverSignUpBtn} className=' bg-green-500'>
+        네이버로 로그인
+      </button>
+      <button onClick={handleKakaoSignUpBtn} className='bg-yellow-300'>
+        카카오로 로그인
+      </button>
+    </div>
   );
 };
 
